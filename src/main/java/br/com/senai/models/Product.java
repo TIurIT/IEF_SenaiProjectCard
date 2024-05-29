8885fc078d707fb1227e8c03a163fe8d80be1a5a
TIuriT <iuriestevam@hotmail.com>
2024-05-28 21:02:52 -0300

    14/05

---
+++ src/main/java/br/com/senai/models/Product.java
@@ -0,0 +1,32 @@
+package br.com.senai.models;
+
+
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name="product")
+@EqualsAndHashCode(of="id")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    @Column(nullable = false)
+    private Double price;
+
+
+}
